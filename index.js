6c89fb191937efd8197a3b8c7eb44622142055c4
felipepineda40academy <felipe.pineda@academiacuatropuntocero.com>
2022-04-29 19:19:55 -0600

    trabajo en clase 29-4

---
+++ index.js
@@ -0,0 +1,6 @@
+//1. Dashboard Nike (No se vale marca Nike)
+
+//2. una funcion que calcule el logaritmo natural de un numero
+
+//3. busqueda simple si el texto incluye playa
+var text = "mañana vamos a comer un churchill al puerto, y luego vamos a la playa";
